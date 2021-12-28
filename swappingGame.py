c6bba3e9bba7b9a514fc78fdea23c4ce9c03d334
Anant278 <71018137+Anant278@users.noreply.github.com>
2021-12-28 19:52:05 +0530

    Add files via upload

---
+++ swappingGame.py
@@ -0,0 +1,20 @@
+def swappingGame():
+    #Input
+    file1 = input("Enter file name: ")
+    file2 = input("Enter other file name: ")
+
+    #Saving
+    with open(file1, 'r') as a:
+        data_a = a.read()
+
+    with open(file2, 'r') as b:
+        data_b = b.read()
+
+    #Rewriting
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    
+    with open(file2, 'w') as b:
+        b.write(data_a)
+        
+swappingGame()
